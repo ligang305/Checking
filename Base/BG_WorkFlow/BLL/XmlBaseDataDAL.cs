3dbeb934f371dbfd7c1dd6d6467fed01991b4c0a
ligang305 <ligang5411@126.com>
2026-03-16 09:09:22 +0800

    Remove copyright summary from XmlBaseDataDAL.cs
    Removed copyright comments from XmlBaseDataDAL.cs

--- Base/BG_WorkFlow/BLL/XmlBaseDataDAL.cs
+++ Base/BG_WorkFlow/BLL/XmlBaseDataDAL.cs
@@ -12,15 +12,6 @@ using CMW.Common.Utilities;
 
 namespace BG_WorkFlow
 {
-    ///<summary>
-    ///--------------------------------------------------
-    /// Copyright (C) 2018-2020 中广核贝谷科技有限公司
-    /// 编写者：zhuzhiwu
-    /// 文件名：XmlBaseDataDAL
-    /// 功能：xml底层实现类
-    /// 编写日期：20181213
-    ///--------------------------------------------------
-    ///</summary>
     public class XmlBaseDataDAL : BaseInstance<XmlBaseDataDAL>
     {
         
